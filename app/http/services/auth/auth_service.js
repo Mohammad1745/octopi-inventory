78bd6c1478f1c75a59eef0db7924cf5075916a3b
ali <mdali2016.227@gmail.com>
2022-08-05 00:28:25 +0600

    [Add] auth service signup method

--- app/http/services/auth/auth_service.js
+++ app/http/services/auth/auth_service.js
@@ -29,7 +29,10 @@ class AuthService extends Service {
      */
     signUp = async (request, response) => {
         try {
-            return this.response().success('hi')
+            const user = new User(this.#_formatUserData(request.body))
+            const userData = await user.save()
+
+            return this.response(userData).success('hi')
         } catch (e) {
             return this.response().error(e.message)
         }
@@ -47,6 +50,30 @@ class AuthService extends Service {
         } catch (e) {
             return this.response().error(e.message)
         }
+    }
+    #_formatUserData = (data) => {
+        return data ?
+            {
+                name: data.name,
+                email: data.email,
+                password: makeHash(data.email,data.password),
+            }
+            : {}
+    }
+    #_authorizeUser = (user, request, response) => {
+        const {id, name, email, } = user
+        const data = {id, name, email}
+        const authToken = jwt.sign(data, process.env.AUTH_SECRET, {expiresIn: SESSION_TIMEOUT+'s'})
+        if (request.headers['content-type'] ==="application/json") {
+            data.authorization = {
+                tokenType: 'Bearer',
+                token: authToken
+            }
+        } else {
+            // Setting the auth token in cookies
+            response.cookie('authToken', authToken)
+        }
+        return data
     }
 }
 
