cfc5217169e2fa8c18a9c41fa7eb12e2086a6440
ali <mdali2016.227@gmail.com>
2022-08-04 21:52:41 +0600

    [Add] app entrypoint

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(cors())
+require('./app/providers/route_service_provider')(app, express)
+
+
+const PORT = process.env.PORT || 8001
+app.listen(PORT, () => console.log(`Server Running on ${PORT}`))
